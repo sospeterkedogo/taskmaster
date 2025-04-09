ca173cd3af7d56cb7fc91bbe74b2715ff518260d
Sospeter <kedogosospeter36@gmail.com>
2025-04-09 15:01:49 +0100

    migrated to postgres db for data persistence

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///test.db')
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
@@ -15,8 +16,8 @@ class Todo(db.Model):
         return 'Task %r' % self.id
 
 with app.app_context():
-    #db.create_all()
-    pass
+    db.create_all()
+    
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
